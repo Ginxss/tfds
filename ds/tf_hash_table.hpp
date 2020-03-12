37f1c625060bc881c4d6614b1b26083002b7764f
Timo Friedrich <timo.friedrich99@gmx.de>
2020-03-12 15:52:44 +0100

    HashTable: djb2 byte hashing

--- ds/tf_hash_table.hpp
+++ ds/tf_hash_table.hpp
@@ -5,37 +5,54 @@
 #ifndef TF_HASH_TABLE_H
 #define TF_HASH_TABLE_H
 
-#include <cstdlib> // malloc
-#include <cstring> // strlen
-#include <iostream>
+#include <cstring> // memcpy
+#include <iostream> // DEBUG
 #include "tf_exception.hpp"
 
 namespace tf {
 
-unsigned long byte_hash(const char *bytes, int length) {
-    unsigned long result = 0;
+// src: http://www.cse.yorku.ca/~oz/hash.html
+// the actual djb2 algorithm which iterates over a string until a '0' is found
+unsigned long string_hash(unsigned char *str) {
+    unsigned long hash = 5381;
+    int c;
 
-    for (int i = 0; i < length; ++i) {
-        result += (i+1) * bytes[i];
+    while ((c = *str++))
+        hash = ((hash << 5) + hash) + c;
+
+    return hash;
+}
+
+// a modified version that iterates over a fixed number of bytes
+unsigned long byte_hash(unsigned char *bytes, int length) {
+    unsigned long hash = 5381;
+    int c;
+
+    int count = 0;
+    while (count++ < length) {
+        c = *bytes++;
+        hash = ((hash << 5) + hash) + c;
     }
 
-    return result;
+    return hash;
 }
 
 template <typename K>
 unsigned long hash(const K &key) {
-    // return byte_hash( (char *)( (void *)&key ) );
-    return byte_hash(static_cast<const char*>(static_cast<const void *>(&key)), sizeof(K));
+    int key_size = sizeof(K);
+    unsigned char bytes[key_size];
+    memcpy(bytes, &key, key_size);
+    return byte_hash(bytes, key_size);
 }
 
 template <>
 unsigned long hash<std::string>(const std::string &key) {
-    return byte_hash(key.c_str(), key.length());
+    return string_hash((unsigned char *)key.c_str());
 }
 
 template <>
 unsigned long hash<const char *>(const char * const &key) {
-    return byte_hash(key, strlen(key));
+    return string_hash((unsigned char *)key);
 }
 
 template <typename K, typename V>
@@ -54,6 +71,15 @@ private:
     const int table_size;
     bucket **buckets;
 
+// FUNCTIONS
+
+    void free_bucket(bucket *b) {
+        if (b) {
+            free_bucket(b->next);
+            free(b);
+        }
+    }
+
 public:
     hash_table(const int table_size = 100):
         table_size(table_size),
@@ -68,6 +94,7 @@ public:
         clear();
     }
 
+    // average: O(1) - worst case: O(n)
     void insert(const K &key, const V &value) {
         unsigned long index = hash<K>(key) % table_size;
         bucket *new_bucket;
@@ -91,6 +118,7 @@ public:
         new_bucket->next = nullptr;
     }
 
+    // average: O(1) - worst case: O(n)
     const V &get(const K &key) const {
         unsigned long index = hash<K>(key) % table_size;
         bucket *it = *(buckets + index);
@@ -106,6 +134,7 @@ public:
         throw tf::exception("hash table: get: invalid key");
     }
 
+    // average: O(1) - worst case: O(n)
     V &operator[](const K &key) {
         unsigned long index = hash<K>(key) % table_size;
         bucket *it = *(buckets + index);
@@ -131,13 +160,6 @@ public:
         buckets = nullptr;
     }
 
-    void free_bucket(bucket *b) {
-        if (b) {
-            free_bucket(b->next);
-            free(b);
-        }
-    }
-
     // DEBUG
     void print() {
         for (int i = 0; i < table_size; ++i) {
@@ -145,7 +167,7 @@ public:
 
             bucket *it = *(buckets + i);
             while (it) {
-                std::cout << it->key << "/" << it->value << " ";
+                std::cout << it->key->a << "/" << it->value << " ";
                 it = it->next;
             }
 
